03bfd003816dbaf4d0b68de687307bb0fd771daa
Itszuvalex <Itszuvalex@gmail.com>
2020-02-22 23:42:54 -0800

    Create ServerChatConnectionData in DataPersistence

--- Minecraft-DiscordBot-Core/Services/DataPersistenceService.cs
+++ Minecraft-DiscordBot-Core/Services/DataPersistenceService.cs
@@ -12,12 +12,14 @@ namespace MinecraftDiscordBotCore.Services
     {
         public BotControlData BotControlData { get; set; }
         public KnownServerData KnownServerData { get; set; }
+        public ServerChatConnectionData ServerChatConnectionData { get; set; }
         public const string FolderName = "Data";
 
         public DataPersistenceService()
         {
             BotControlData = new BotControlData();
             KnownServerData = new KnownServerData();
+            ServerChatConnectionData = new ServerChatConnectionData();
             if(!Directory.Exists(FolderName))
             {
                 Directory.CreateDirectory(FolderName);
@@ -26,9 +28,11 @@ namespace MinecraftDiscordBotCore.Services
             {
                 Load<BotControlData, Dictionary<string, HashSet<ulong>>>(BotControlData.DataKey, BotControlData);
                 Load<KnownServerData, Dictionary<string, string>>(KnownServerData.DataKey, KnownServerData);
+                Load<ServerChatConnectionData, Dictionary<string, List<Tuple<ulong, ulong>>>>(ServerChatConnectionData.DataKey, ServerChatConnectionData);
             }
             BotControlData.DataPersistence = this;
             KnownServerData.DataPersistence = this;
+            ServerChatConnectionData.DataPersistence = this;
         }
 
 
